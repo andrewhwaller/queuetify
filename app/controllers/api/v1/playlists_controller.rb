7e5037df28750041422daae981d4fe8acc6e1864
andrewhwaller <andrewhwaller@gmail.com>
2019-09-29 17:32:54 -0500

    Build out test playlist model with controller

---
+++ app/controllers/api/v1/playlists_controller.rb
@@ -0,0 +1,41 @@
+class Api::V1::PlaylistsController < ApplicationController
+
+  def index
+    @playlists = Playlist.all
+
+    render json: @playlists, status: 200
+  end
+
+  def show
+    @playlist = Playlist.find(params[:id])
+
+    render json: @playlist, status: 200
+  end
+
+  def create
+    @playlist = Playlist.create(playlist_params)
+
+    render json: @playlist, status: 200
+  end 
+
+  def update
+    @playlist = Playlist.find(params[:id])
+    @playlist.update(playlist_params)
+    render json: @playlist, status: 200
+  end
+
+  def destroy
+    @playlist = Playlist.find(params[:id])
+    @playlist.delete
+
+    render json: {playlistId @playlist.id}
+  end
+
+  private
+
+  def playlist_params
+    params.require(:playlist).permit(:name)
+  end
+    
+
+end
