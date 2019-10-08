6318d8845d895a383f0d4770127b6ce14810f08d
andrewhwaller <andrewhwaller@gmail.com>
2019-10-07 21:34:08 -0500

    Build out track methods

--- queuetify-api/app/controllers/api/v1/tracks_controller.rb
+++ queuetify-api/app/controllers/api/v1/tracks_controller.rb
@@ -1,2 +1,15 @@
 class Api::V1::TracksController < ApplicationController
+
+  def index
+    @tracks = Track.all
+    render json: @tracks
+  end
+
+  def top_100
+    s_tracks = RSpotify::Playlist.find("1276640268","2kpoUUJ5a4Cw3feTkFJhZ2").tracks
+    @tracks = s_tracks.map do |s_track|
+      Track.new_from_spotify_track(s_track)
+    end
+    render json: @tracks
+  end
 end
