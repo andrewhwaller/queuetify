c01304f21d9e3e79066ca43a75f43d15eadd9042
andrewhwaller <andrewhwaller@gmail.com>
2019-10-08 23:11:21 -0500

    Update playlists controller with initial methods

--- queuetify-api/app/controllers/api/v1/playlists_controller.rb
+++ queuetify-api/app/controllers/api/v1/playlists_controller.rb
@@ -1,26 +1,28 @@
 class Api::V1::PlaylistsController < ApplicationController
+  before_action :set_user
 
   def index
-    playlists = Playlist.all
+    playlists = @user.playlists
 
     render json: playlists, status: 200
   end
 
   def show
-    playlist = Playlist.find(params[:id])
+    playlist = RSpotify::Playlist.find(user_id, playlist_id)
 
     render json: playlist, status: 200
   end
 
   def create
-    playlist = Playlist.create(playlist_params)
+    playlist = @user.create_playlist!(playlist_name)
 
     render json: playlist, status: 200
   end 
 
   def update
-    playlist = Playlist.find(params[:id])
-    playlist.update(playlist_params)
+    playlist = RSpotify::Playlist.find(user_id, playlist_id)
+    tracks = RSpotify::Track.search(search_term)
+    playlist.add_tracks!(tracks)
     render json: playlist, status: 200
   end
 
@@ -36,6 +38,8 @@ class Api::V1::PlaylistsController < ApplicationController
   def playlist_params
     params.require(:playlist).permit(:name)
   end
-    
 
+  def set_user
+    @user = RSpotify::User.new(request.env['omniauth.auth'])
+  end
 end
